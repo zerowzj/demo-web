3c63ac03ff180f17424464210a22703effc4d00c
wangzhenjun <wangzhenjun@okay.cn>
2018-07-06 18:31:10 +0800

    更新代码

---
+++ doc/start-web.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PROJECT_NAME=demo-web
+
+export JAVA_HOME=/usr/jdk1.8.0_162
+export PATH=$PATH:$JAVA_HOME/bin
+
+pid=`ps -ef |grep $PROJECT_NAME |grep -v grep |awk '{print $2}'`
+
+if [ -n "$pid" ]; then
+    kill -9 $pid
+    echo "------------------------------"
+    echo "     Shutdown tomcat...       "
+    echo "------------------------------"
+fi
+
+sh ./startup.sh
+
+
