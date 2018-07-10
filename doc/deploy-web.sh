f2ad87160fc8f6ac64b87c6172ef1581eba38780
wangzhenjun <wangzhenjun@okay.cn>
2018-07-10 14:53:36 +0800

    更新代码

--- doc/deploy-web.sh
+++ doc/deploy-web.sh
@@ -5,21 +5,20 @@ DEPLOY_HOME=/app/demo-web
 WEB_APPS=$DEPLOY_HOME/webapps
 ROOT=$WEB_APPS/ROOT
 
-war_file=$1
-echo "=====>$1"
-if [ ! -f "$DEPLOY_HOME/$war_file" ]; then
-    echo "文件不存在"
+WAR_FILE=$DEPLOY_HOME/$1
+if [ ! -f "$WAR_FILE" ]; then
+    echo "ERROR: file[$WAR_FILE] not exist!"
     exit 1
 fi
 
 cd `dirname $0`
-#进入webapps
-cd ..
+##进入webapps
+#cd ..
 
 rm -rf $ROOT
-unzip -oq $war_file -d $ROOT;
+unzip -oq $WAR_FILE -d $ROOT;
 sleep 3;
-rm -rf $war_file
+rm -rf $WAR_FILE
 
 pid=`ps -ef |grep /app/demo-web/ |grep -v grep |grep -v deploy-web.sh |awk '{print $2}'`
 if [ -n "$pid" ]; then
@@ -28,3 +27,5 @@ if [ -n "$pid" ]; then
     echo "     Shutdown tomcat...       "
     echo "------------------------------"
 fi
+
+sh startup.sh
