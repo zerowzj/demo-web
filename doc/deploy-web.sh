3f09f9d74741387ec83221d047fea22b6c75c2d1
wangzhenjun <wangzhenjun@okay.cn>
2018-07-10 14:42:32 +0800

    更新代码

--- doc/deploy-web.sh
+++ doc/deploy-web.sh
@@ -3,31 +3,28 @@
 PROJECT_NAME=demo-web
 DEPLOY_HOME=/app/demo-web
 WEB_APPS=$DEPLOY_HOME/webapps
+ROOT=$WEB_APPS/ROOT
 
 war_file=$1
+echo "=====>$1"
 if [ ! -f "$DEPLOY_HOME/$war_file" ]; then
     echo "文件不存在"
     exit 1
 fi
 
 cd `dirname $0`
+#进入webapps
 cd ..
 
-rm -rf $WEB_APPS/ROOT
-unzip -oq $war_file -d $WEB_APPS/ROOT;
+rm -rf $ROOT
+unzip -oq $war_file -d $ROOT;
 sleep 3;
 rm -rf $war_file
 
-pid=`ps -ef |grep $PROJECT_NAME |grep -v grep |awk '{print $2}'`
+pid=`ps -ef |grep /app/demo-web/ |grep -v grep |grep -v deploy-web.sh |awk '{print $2}'`
 if [ -n "$pid" ]; then
     kill -9 $pid
     echo "------------------------------"
     echo "     Shutdown tomcat...       "
     echo "------------------------------"
 fi
-
-export JAVA_HOME=/usr/jdk1.8.0_162
-export PATH=$PATH:$JAVA_HOME/bin
-sh ./startup.sh
-
-
